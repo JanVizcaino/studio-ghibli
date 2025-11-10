f9b0a8231098eb2ecfee3b466929b3af998df28d
JanVizcaino <jan.vizcaino@outlook.es>
2025-11-10 18:32:31 +0100

    primer commit del proyecto

---
+++ ghibli/src/App.jsx
@@ -0,0 +1,12 @@
+import Greeting from "./components/Greeting.jsx";
+import Counter from "./components/Counter.jsx";
+function App() {
+  return (
+    <main>
+      <h1>Ghibli App</h1>
+      <Greeting name="DAW1" />
+      <Counter />
+    </main>
+  );
+}
+export default App;
