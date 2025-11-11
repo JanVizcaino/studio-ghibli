592a25c6ad613aac5ddce7b8182fc44b82e737dd
JanVizcaino <jan.vizcaino@outlook.es>
2025-11-11 19:31:04 +0100

    Fase 10-Acabada+Tailwind

--- ghibli/src/App.jsx
+++ ghibli/src/App.jsx
@@ -1,18 +1,14 @@
-// 1) Traemos NavLink (enlaces reactivos) y el mapeo de rutas (Routes/Route) 
 import { NavLink, Routes, Route } from 'react-router-dom' 
-
-// 2) Traemos las páginas 
 import Home from './pages/Home.jsx' 
 import Characters from './pages/Characters.jsx' 
 import Contact from './pages/Contact.jsx' 
 import Game from './pages/Game.jsx' 
+import FilmDetail from './pages/FilmDetail.jsx' // NUEVO 
 
-// 3) Estructura principal con cabecera (nav) y el switch de rutas (Routes) 
 export default function App() { 
   return ( 
-    <> 
+    <>  
       <header> 
-        {/* Menú simple: NavLink se marca "active" cuando la ruta coincide */} 
         <nav> 
           <NavLink to="/">Home</NavLink> 
           <NavLink to="/characters">Characters</NavLink> 
@@ -21,10 +17,11 @@ export default function App() {
         </nav> 
       </header> 
 
-      {/* Según la URL, se renderiza uno de estos elementos */} 
       <Routes> 
         <Route path="/" element={<Home />} /> 
         <Route path="/characters" element={<Characters />} /> 
+        {/* Ruta dinámica: :id */} 
+        <Route path="/characters/:id" element={<FilmDetail />} /> 
         <Route path="/contact" element={<Contact />} /> 
         <Route path="/game" element={<Game />} /> 
       </Routes> 
